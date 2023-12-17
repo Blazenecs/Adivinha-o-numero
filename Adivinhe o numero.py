65064c52c987da9e6faef43ca30b33be41c29d6c
Blazenecs <110887997+Blazenecs@users.noreply.github.com>
2023-12-17 19:34:42 -0300

    Update Adivinhe o numero.py

--- Adivinhe o numero.py
+++ Adivinhe o numero.py
@@ -1,15 +1,1 @@
-import random 
 
-num_min = 1
-num_max = 100
-chutes = 0
-
-def random_number(num_min, num_max):
-    while True:
-        chute = int(input("Digite um numero de 1 a 100: "))
-        chutes + 1
-        if chute == (random_number - 10) or chute == (random_number + 10):
-            print("Voce esta a 10 numeros de distancia para acertar o numero!")
-        elif chute == random_number:
-            print(f"Parabens voce acertou o numero! com {chutes} chutes")
-        break    
